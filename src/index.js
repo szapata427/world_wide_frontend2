524e4a76fc598ae0597ccf38b1723342d7a4ae98
Carolyn Rosemary Winifred Cole <carolynrosemarywinifredcole@Carolyns-MacBook-Air.local>
2018-11-16 13:32:15 -0500

    got the search map to work

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const mapLocation = document.getElementById('map')
 const countryContainer = document.getElementById('liked-countries')
 let infoWindow;
 let datalistOptions = document.querySelector('datalist[id="browsers"]')
-
-
+const search = document.querySelector('.search')
+let foundMarker;
 
 
  function initMap(){
@@ -28,7 +28,7 @@ let datalistOptions = document.querySelector('datalist[id="browsers"]')
      scaledSize: new google.maps.Size(30, 30)
    };
    //Adding Marker
-   let marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     position:{lat: country['location_lat'] ,lng: country['location_lng'] },
     map:map,
     title: country["name"],
@@ -42,7 +42,7 @@ let datalistOptions = document.querySelector('datalist[id="browsers"]')
 
 
    marker.addListener('mouseover', function(event) {
-     let countryPopulation = Number(parseFloat(country["population"]).toFixed(2)).toLocaleString('en', {
+     let countryPopulationone = Number(parseFloat(country["population"]).toFixed(2)).toLocaleString('en', {
       minimumFractionDigits: 0
           });
 
@@ -50,7 +50,7 @@ let datalistOptions = document.querySelector('datalist[id="browsers"]')
     infoWindow = new google.maps.InfoWindow({
      content: `<div class="country-info"><h4 class="country-title">${country["name"]}<h4>
           <p><span class="info-object"> Native Name:</span> <span class="info-style">${country["native_name"]}</span></p>
-          <p><span class="info-object">Population:</span> <span class="info-style">${countryPopulation}</span></p>
+          <p><span class="info-object">Population:</span> <span class="info-style">${countryPopulationone}</span></p>
           <p><span class="info-object">Official Language:</span> <span class="info-style">${country["language"]}</span></p>
           <p><span class="info-object">Capital:</span> <span class="info-style">${country["capital"]}</span></p>
           <p><span class="info-object">Currency:</span> <span class="info-style">${country["currency_name"]}</span></p>
@@ -71,9 +71,6 @@ let datalistOptions = document.querySelector('datalist[id="browsers"]')
 })
 }
 
-function repopMap() {
-    console.log("hellue")
-}
 
 //iterating through favorite_countries database and pushing the liked country_ids to the array
 let likedCountriesArray = []
@@ -217,27 +214,103 @@ return counter
 
 
 
+// populating datalist
+fetch(`http://localhost:3000/countries`)
+  .then(response => response.json())
+  .then(json => {
+    json.forEach(country => {
+      datalistOptions.innerHTML += `<option value="${country.name}"><span id="${country.id}"></span>`
+      search.addEventListener('submit', repop)
+     })
+    })
+// })
 
-fetch(`http://localhost:3000/countries`).then(response => response.json())
-.then(json => {
-  json.forEach(country => {
-    datalistOptions.innerHTML += `<option value="${country.language}"><span id="${country.id}"></span>`
+function repop(event){
 
-
-  })
-
-})
-
-
-
-const form = document.querySelector('#browsers')
-// console.log(form);
-form.addEventListener("click", event => {
-  // if (event.target)
-  debugger
   event.preventDefault()
-  console.log("in the event listener");
+  let input = document.querySelector('input')
+
+    fetch('http://localhost:3000/countries')
+      .then(res => res.json())
+      .then(function(json){
+     // console.log(json)
+     let foundCountry = json.find(function(country){
+       return country.name == input.value
+     })
+
+     let map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: foundCountry['location_lat'], lng: foundCountry['location_lng']},
+        zoom: 4
+       });
+
+  //Adding Marker
+  // if (input.value === country.name){
+    let image = {
+     url: foundCountry.flag,
+     // This marker is 20 pixels wide by 32 pixels high.
+     size: new google.maps.Size(40, 30),
+     origin: new google.maps.Point(0, 0),
+      // The anchor for this image is the base of the flagpole at (0, 32).
+      anchor: new google.maps.Point(0, 32),
+      scaledSize: new google.maps.Size(30, 30)
+    };
+
+   foundMarker = new google.maps.Marker({
+     position:{ lat: foundCountry['location_lat'] ,lng: foundCountry['location_lng'] },
+     map:map,
+     title: foundCountry["name"],
+   //size: new google.maps.Size(2, 3),
+     icon: image
+    });
+
+  foundMarker.addListener('click', function(event) {
+    let countryPopulation = Number(parseFloat(foundCountry["population"]).toFixed(2)).toLocaleString('en', {
+     minimumFractionDigits: 0
+   })
+
+   // create a new infoWindow for the foundMarker
+
+     if (infoWindow) infoWindow.close();
+     infoWindow = new google.maps.InfoWindow({
+      content: `<div class="country-info"><h4 class="country-title">${foundCountry["name"]}<h4>
+           <p><span class="info-object"> Native Name:</span> <span class="info-style">${foundCountry["native_name"]}</span></p>
+           <p><span class="info-object">Population:</span> <span class="info-style">${countryPopulation}</span></p>
+           <p><span class="info-object">Official Language:</span> <span class="info-style">${foundCountry["language"]}</span></p>
+           <p><span class="info-object">Capital:</span> <span class="info-style">${foundCountry["capital"]}</span></p>
+           <p><span class="info-object">Currency:</span> <span class="info-style">${foundCountry["currency_name"]}</span></p>
+           <p><span class="info-object">Currency Symbol:</span><span class="info-style">${foundCountry["currency_symbol"]}</span></p>
+           <a href="https://en.wikipedia.org/wiki/${foundCountry["native_name"]}"> More Info </a> <span><button data-id="${foundCountry["id"]}" id="like_btn" class="btn btn-primary"> Like </button></span>
+      </div>`
+     })
+     infoWindow.open(map, foundMarker);
+     // infoWindow
+     // tried to get the parentelment, add a class and use css on the class
+     // let countryInfo = document.querySelector(".gm-style-iw").parentElement
+     // countryInfo.className += "map-country-info";
+    })
+
+
+  //Opening info window
+   //console.log(country["latlng"][0])
+  //Added Marker?
+// }
+
+
+  //End of marker
 })
+// )
+}
+
+
+
+// const form = document.querySelector('#browsers')
+// // console.log(form);
+// form.addEventListener("click", event => {
+//   // if (event.target)
+//   debugger
+//   event.preventDefault()
+//   console.log("in the event listener");
+// })
 // })
 
 // function to find the most common string in an array
